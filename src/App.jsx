4cdc45fa050e8010484bb10b7315e42573bee3d6
Diwakar Bhardwaj <diwakarraj1245@gmail.com>
2025-04-13 14:57:43 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-import Register from './components/Register';
-import Login from './components/Login';
-import './App.css';
-import UpdatePassword from './components/UpdatePassword';
+import React from 'react'
+import Contact from './pages/Contact'
+import Register from './pages/Register'
+import Services from './pages/Services'
+import About from './pages/About'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import { Routes,Route } from 'react-router-dom'
 
-import { BrowserRouter, Routes, Route ,Link} from "react-router-dom";
+
+
+import Cardiology from './specialisations/Cardiology'
+import Dental from './specialisations/Dental'
+import Dermatology from './specialisations/Dermatology'
+import Neurology from './specialisations/Neurology'
+import Orthopedics from './specialisations/Orthopedics'
+import Footer from './components/Footer'
+import Login from './components/Login'
+// import Register from './components/Register'
+import UpdatePassword from './components/UpdatePassword'
+import Appointments from './pages/Appointments'
+
+
 
 const App = () => {
   return (
-    <>
     <div>
+      
+      <Navbar />
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/services' element = {<Services />} />
+        <Route path='/pages' element = {<Register />} />
+        <Route path='/pages/login' element={<Login />} />
+        {/* <Route path='/pages' element={<Register />} /> */}
+        <Route path = '/pages/update_password' element={<UpdatePassword />} />
+        
+        <Route path='/contact' element = {<Contact />} />
+        <Route path='/appointments' element = {<Appointments />} />
+        
+        <Route path='/Cardiology' element = {<Cardiology />} /> 
+        <Route path='Dental' element = {<Dental />} />  
+        <Route path='/Dermatology' element = {<Dermatology />} /> 
+        <Route path='/Neurology' element = {<Neurology />} />
+        <Route path='/Orthopedics' element = {<Orthopedics />} />
 
+      </Routes>
+      <Footer />
     </div>
-    <div className='app-wrapper'>
-     
-      <BrowserRouter>
-      <nav>
-                <ul>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/update-password">Update Password</Link></li>
-                </ul>
-            </nav>
-            <Routes>
-                {/* other routes */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/update-password" element={<UpdatePassword />} />
-            </Routes>
-        </BrowserRouter>
-    </div>
-    </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-// main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client'
+import './App.css'
+import './index.css'
+
+
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AppointmentProvider } from './context/AppointmentContext.jsx' 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+  <BrowserRouter>
+    <AppointmentProvider>
+      <App />
+    </AppointmentProvider>
+  </BrowserRouter>
+)
